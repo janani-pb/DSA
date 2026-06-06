2412171097f25e068e169c22908b7dc56a255608
janani-pb <jananipb.24cse@kongu.edu>
2026-06-06 09:51:07 +0530

    Time: 4 ms (18.58%), Space: 47.6 MB (72.09%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,43 +1,66 @@
 class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
     public void solve(char[][] board) {
-        int c=0;
         int row = board.length;
         int col = board[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        boolean [][] visited=new boolean[row][col];
         for(int j=0;j<col;j++){
             if(board[0][j]=='O'){
-                dfs(board,0,j,row,col);
+                board[0][j]='#';
+                q.offer(new Pair(0,j));
+                visited[0][j]=true;
             }
             if(board[row-1][j]=='O'){
-                dfs(board,row-1,j,row,col);
+                board[row-1][j]='#';
+                q.offer(new Pair(row-1,j));
+                visited[row-1][j]=true;
             }
         }
         for(int i=0;i<row;i++){
             if(board[i][0]=='O'){
-                dfs(board,i,0,row,col);
+                board[i][0]='#';
+                q.offer(new Pair(i,0));
+                visited[i][0]=true;
             }
             if(board[i][col-1]=='O'){
-                dfs(board,i,col-1,row,col);
+                board[i][col-1]='#';
+                q.offer(new Pair(i,col-1));
+                visited[i][col-1]=true;
             }
         }
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(board[i][j]=='O'){
-                    board[i][j]='X';
+        int[] drow={-1,1,0,0};
+        int[] dcol={0,0,1,-1};
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            int rrow=curr.row;
+            int rcol=curr.col;
+            for(int i=0;i<4;i++){
+                int ro=rrow+drow[i];
+                int co=rcol+dcol[i];
+                if(ro>=0 && co>=0 && ro<row && co<col && !visited[ro][co] && board[ro][co]=='O'){
+                    board[ro][co]='#';
+                    visited[ro][co]=true;
+                    q.offer(new Pair(ro,co));
                 }
-                if(board[i][j]=='#'){
-                    board[i][j]='O';
-                }
+        }
+    }
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(board[i][j]=='O'){
+                board[i][j]='X';
+            }
+            if(board[i][j]=='#'){
+                board[i][j]='O';
             }
         }
     }
-    public void dfs(char[][] board,int i,int j,int row,int col){
-        if(i<0 || j<0 || i>=row || j>=col || board[i][j]!='O'){
-            return;
-        }
-        board[i][j]='#';
-        dfs(board,i-1,j,row,col);
-        dfs(board,i+1,j,row,col);
-        dfs(board,i,j-1,row,col);
-        dfs(board,i,j+1,row,col);
     }
 }
