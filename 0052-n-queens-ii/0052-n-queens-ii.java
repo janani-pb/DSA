6863d9d633bba61352ec5d90bc5141682e0b2b10
janani-pb <jananipb.24cse@kongu.edu>
2026-06-09 11:46:31 +0530

    Time: 2 ms (57.57%), Space: 42.7 MB (14.64%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int totalNQueens(int n) {
+        ArrayList<List<String>> al = new ArrayList<>();
+        char[][] q = new char[n][n];
+        for(int i=0;i<q.length;i++){
+            Arrays.fill(q[i],'.');
+        }
+        solveuntil(q,al,0);
+        return al.size();
+    }
+    public boolean isvalid(char[][]q,int row,int col){
+        //left check
+        for(int i=col-1;i>=0;i--){
+            if(q[row][i]=='Q'){
+                return false;
+            }
+        }
+        //top left check
+        for(int i=row-1,j=col-1;i>=0 && j>=0 ;i--,j--){
+            if(q[i][j]=='Q'){
+                return false;
+            }
+        }
+        //bottom left diagonal
+        for(int i=row+1,j=col-1;i<q.length&&j>=0;i++,j--){
+            if(q[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public void solveuntil(char[][]q,ArrayList<List<String>>al,int index){
+        if(index==q.length){
+            ArrayList<String> temp=new ArrayList<>();
+            for(int i=0;i<q.length;i++){
+                StringBuilder sb = new StringBuilder();
+                for(int j=0;j<q.length;j++){
+                    sb.append(q[i][j]);
+                }
+                temp.add(sb.toString());
+            }
+            al.add(temp);
+            return;
+        }
+        for(int i=0;i<q.length;i++){
+            if(isvalid(q,i,index)){
+                q[i][index]='Q';
+                solveuntil(q,al,index+1);
+                q[i][index]='.';
+            }
+        }
+    }}
+
