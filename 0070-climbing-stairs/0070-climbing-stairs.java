34ea1ae496b9aa77d6208fd898b7c51a81191c57
janani-pb <jananipb.24cse@kongu.edu>
2026-06-11 11:22:27 +0530

    Time: 0 ms (100%), Space: 42.1 MB (34.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,14 @@
 class Solution {
-    public static int fib(int n,int[] dp){
-	    if(n<=3){
-	        return n;
-	    }
-	   if(dp[n]!=-1){
-	       return dp[n];
-	   }
-	   dp[n]=fib(n-1,dp)+fib(n-2,dp);
-	   return dp[n];
-	}
     public int climbStairs(int n) {
-        int [] dp = new int [n+1];
-		Arrays.fill(dp,-1);
-		
-		int f= fib(n,dp);
- 		return f;
+        if(n==0 || n==1){
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
