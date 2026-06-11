39d36c2fdba78a9625f112254b1bb0f46ba86be0
janani-pb <jananipb.24cse@kongu.edu>
2026-06-11 11:15:17 +0530

    Time: 0 ms (100%), Space: 42.1 MB (32.8%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,18 @@
 class Solution {
-    public int fib(int n) {
+    public int fibo (int n, int[] dp){
         if(n==1 || n==0){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+        return dp[n];
+    }
+    public int fib(int n) {
+        int [] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        int f= fibo(n,dp);
+        return f;
     }
 }
