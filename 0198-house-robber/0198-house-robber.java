5d193d5896f0a1fbf1c7002aa72e4f8a2e26fe8e
janani-pb <jananipb.24cse@kongu.edu>
2026-06-11 14:45:21 +0530

    Time: 0 ms (100%), Space: 43 MB (18.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n==1){
-            return nums[0];
+        int t1=0;
+        int t2=0;
+        for(int num:nums){
+            int t=Math.max(t1,num+t2);
+            t2=t1;
+            t1=t;
         }
-        int []dp = new int [n];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
-        }
-        return dp[n-1];
+        return t1;
     }
 }
