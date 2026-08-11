be53653e18c87b4bb341b6e0943713f28207e8a9
Janani PB <jananipb.24cse@kongu.edu>
2026-08-11 21:16:58 +0530

    Time: 1 ms (92.61%), Space: 42.4 MB (36.72%) - LeetHub

---
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+        String s = String.valueOf(num);
+        int c=0;
+        for(int i=0;i<=s.length()-k;i++){
+            String sub = s.substring(i,i+k);
+            int n1=Integer.parseInt(sub);
+            if(n1!=0 && num%n1==0){
+                c++;
+            }
+        }
+        return c;
+    }
+}
